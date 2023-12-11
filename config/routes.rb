2f8db0e820a61dca8bd0164f56bae2fe237a9ba2
jcampbell57 <jcampbell57@gmail.com>
2023-12-11 00:36:55 -0800

    generate posts controller, set root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  root 'posts#index'
+
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/new'
+  get 'posts/create'
   get 'users/index'
   get 'users/show'
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
