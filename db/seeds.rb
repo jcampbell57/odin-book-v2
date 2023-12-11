1696ce05063519f7400e86690b7270239f0ae67f
jcampbell57 <jcampbell57@gmail.com>
2023-12-11 00:48:58 -0800

    add fake users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(fname: 'Humble', lname: 'Bragger', email: 'humblebragger@humblebrag.com', password: 'humblebaby')
+User.create(fname: 'Fitness', lname: 'Grampa', email: 'fitnessgrampacer@test.com', password: 'pacertest')
