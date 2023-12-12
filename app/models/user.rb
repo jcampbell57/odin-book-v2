17999c2fed88f3e70b2a40d6be4510ad1960bc10
jcampbell57 <jcampbell57@gmail.com>
2023-12-11 20:27:28 -0800

    generate friendship model, configure associations, migrate database

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,28 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  # BEGIN FRIENDSHIP ASSOCIATIONS
+  has_many :sent_friend_requests, class_name: 'Friendship',
+                                  foreign_key: 'sent_by_id',
+                                  inverse_of: 'sent_by',
+                                  dependent: :destroy
+  has_many :recieved_friend_requests, class_name: 'Friendship',
+                                      foreign_key: 'sent_to_id',
+                                      inverse_of: 'sent_to',
+                                      dependent: :destroy
+  has_many :friends, -> { merge(Friendship.friends) },
+           through: :sent_friend_requests, source: :sent_to
+  # Not sure if this one is functional:
+  # (not needed because all friendships have two records,
+  # so previous assosiaction will catch all)
+  # has_many :friends, -> { merge(Friendship.friends) },
+  #          through: :recieved_friend_requests, source: :sent_by
+  has_many :pending_requests_sent, -> { merge(Friendship.not_friends) },
+           through: :sent_friend_requests, source: :sent_to
+  has_many :pending_requests_received, -> { merge(Friendship.not_friends) },
+           through: :recieved_friend_requests, source: :sent_by
+  # END FRIENDSHIP ASSOCIATIONS
+
   private
 
   # Validates the size of an uploaded picture.
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_12_025039) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_12_031157) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_12_025039) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "sent_to_id", null: false
+    t.bigint "sent_by_id", null: false
+    t.boolean "status", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["sent_by_id"], name: "index_friendships_on_sent_by_id"
+    t.index ["sent_to_id"], name: "index_friendships_on_sent_to_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -60,6 +70,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_12_025039) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "friendships", "users", column: "sent_by_id"
+  add_foreign_key "friendships", "users", column: "sent_to_id"
   add_foreign_key "likes", "comments"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
