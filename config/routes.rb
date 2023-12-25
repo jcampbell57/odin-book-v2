e835a8a5d5a51e3ba12bd54e775c09305ab4fc96
jcampbell57 <jcampbell57@gmail.com>
2023-12-24 17:54:33 -0800

    update route to display notifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,10 @@
 Rails.application.routes.draw do
-  # get 'friendships/create'
-  # get 'likes/create'
-  # get 'comments/new'
-  # get 'comments/create'
-  # get 'posts/index'
-  # get 'posts/show'
-  # get 'posts/new'
-  # get 'posts/create'
-  # get 'users/index'
-  # get 'users/show'
-
   root 'posts#index'
 
   devise_for :users
 
-  resources :users, only: %i[index show] do
+  resources :users, only: %i[index show notifications] do
+    get :notifications, on: :member
     resources :friendships, only: %i[create destroy] do
       collection do
         get 'accept_friend'
